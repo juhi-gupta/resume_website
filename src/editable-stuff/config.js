72e87c1190a6ce5516e3b94fb8cf4323366aa83b
Juhi <juhi.iacm@gmail.com>
2024-02-24 21:10:53 +0000

    updated project for juhi

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -6,86 +6,39 @@ const navBar = {
 // Main Body SECTION
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
-  firstName: "Sunaina",
+  firstName: "Juhi",
   middleName: "",
-  lastName: "Srivastava",
+  lastName: "Gupta",
   message: " Passionate about changing the world with technology. ",
   icons: [
     {
       image: "fa-github",
-      url: "https://github.com/arjun2010-ece",
-    },
-    {
-      image: "fa-facebook",
-      url: "https://www.facebook.com/arjuncool.17",
+      url: "https://github.com/juhi-gupta",
     },
     {
       image: "fa-linkedin",
-      url: "https://www.linkedin.com/in/arjun-sah-465a4a56/",
+      url: "https://www.linkedin.com/in/juhi-gupta-67a9aa85/",
     },
   ],
 };
 
-// ABOUT SECTION
-// If you want the About Section to show a profile picture you can fill the profilePictureLink either with:
-//a) your Instagram username
-//      i.e:profilePictureLink:"johnDoe123",
-//b) a link to an hosted image
-//      i.e:profilePictureLink:"www.picturesonline.com/johnDoeFancyAvatar.jpg",
-//c) image in "editable-stuff" directory and use require("") to import here,
-//      i.e: profilePictureLink: require("../editable-stuff/hashirshoaeb.png"),
-//d) If you do not want any picture to be displayed, just leave it empty :)
-//      i.e: profilePictureLink: "",
-// For Resume either provide link to your resume or import from "editable-stuff" directory
-//     i.e resume: require("../editable-stuff/resume.pdf"),
-//         resume: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
 
 const about = {
   show: true,
   heading: "About Me",
-  imageLink: require("../editable-stuff/arjun.jpg"),
-  // imageLink: require("../editable-stuff/hashirshoaeb.png"),
+  imageLink: require("../editable-stuff/juhi.jpeg"),
   imageSize: 375,
-  message: "My name is Sunaina Srivastava. I’m have done my Masters in Software engineering from University of Porto, Portugal. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like to continously upgrade myself.",
-  resume: "https://docs.google.com/document/d/1IEJIKlmokNxd2FcXRoB0um_RfJK_haxf4tDVqzRDlDI/edit?usp=sharing",
+  message: "My name is Juhi Gupta. I have done my Masters of Computer Application (MCA) from Punjab Technical University, India. I'm most passionate about giving back to the community, and my goal is to pursue this passion within the field of software engineering. In my free time I like to continously upgrade myself.",
+  resume: "https://docs.google.com/document/d/1hbOxIMOkS-4bHf0kIYOQMNLI6DGJAkVi/edit?usp=sharing&ouid=116043145104145956620&rtpof=true&sd=true",
 };
 
-// PROJECTS SECTION
-// Setting up project lenght will automatically fetch your that number of recently updated projects, or you can set this field 0 to show none.
-//      i.e: reposLength: 0,
-// If you want to display specfic projects, add the repository names,
-//      i.e ["repository-1", "repo-2"]
 const repos = {
   show: true,
   heading: "Recent Projects",
   desc: "I always keeps on working on some personal projects in my free time to update myself. Please find some of my latest sample projects.",
-  gitHubUsername: "arjun2010-ece", //i.e."johnDoe12Gh"
-  reposLength: 7,
+  gitHubUsername: "juhi-gupta", //i.e."johnDoe12Gh"
+  reposLength: 5,
   specificRepos: [],
-};
-
-// Leadership SECTION
-const leadership = {
-  show: false,
-  heading: "Leadership",
-  message:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae auctor eu augue ut lectus arcu bibendum at varius. Libero justo laoreet sit amet cursus sit amet. Imperdiet dui accumsan sit amet nulla facilisi morbi. At auctor urna nunc id. Iaculis urna id volutpat lacus laoreet non curabitur gravida. Et magnis dis parturient montes nascetur ridiculus mus mauris. In nisl nisi scelerisque eu ultrices vitae auctor. Mattis nunc sed blandit libero volutpat sed cras ornare. Pulvinar neque laoreet suspendisse interdum consectetur libero.",
-  images: [
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "First slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-    { 
-      img: require("../editable-stuff/hashirshoaeb.png"), 
-      label: "Second slide label", 
-      paragraph: "Nulla vitae elit libero, a pharetra augue mollis interdum." 
-    },
-  ],
-  imageSize: {
-    width:"615",
-    height:"450"
-  }
 };
 
 // SKILLS SECTION
@@ -95,17 +48,20 @@ const skills = {
   hardSkills: [
     { name: "JavaScript", value: 90 },
     { name: "Typescript", value: 90 },
-    { name: "React", value: 90 },
-    { name: "GatsbyJs", value: 80 },
-    { name: "NextJs", value: 80 },
+    { name: "Angular - All versions", value: 90 },
+    { name: "Angular Material", value: 80 },
+    { name: "NgRx", value: 80 },
+    { name: "RxJS", value: 90 },
     { name: "HTML/CSS", value: 90 },
-    { name: "ExpressJs", value: 75 },
-    { name: "Python", value: 65 },
-    { name: "SQL", value: 65 },
+    { name: "Bootstrap", value: 90 },
+    { name: "Node.Js", value: 75 },
+    { name: "SQL", value: 80 },
+    { name: "Oracle Server", value: 80 },
   ],
   softSkills: [
+    { name: "Good-Communication", value: 80 },
     { name: "Goal-Oriented", value: 80 },
-    { name: "Collaboration", value: 90 },
+    { name: "Team Collaboration", value: 90 },
     { name: "Positivity", value: 75 },
     { name: "Adaptability", value: 85 },
     { name: "Problem Solving", value: 75 },
@@ -120,8 +76,8 @@ const getInTouch = {
   show: true,
   heading: "Get In Touch",
   message:
-    "I'm currently looking for full-time Frontend developer or Fullstack developer opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
-  email: "arjun2010.ece@gmail.com",
+    "I'm currently looking for full-time Frontend angular developer  opportunities! If you know of any positions available, if you have any questions, or if you just want to say hi, please feel free to email me at",
+  email: "juhi.iacm@gmail.com",
 };
 
 const experiences = {
@@ -141,9 +97,4 @@ const experiences = {
   ]
 }
 
-// Blog SECTION
-// const blog = {
-//   show: false,
-// };
-
-export { navBar, mainBody, about, repos, skills, leadership, getInTouch, experiences };
+export { navBar, mainBody, about, repos, skills, getInTouch, experiences };
