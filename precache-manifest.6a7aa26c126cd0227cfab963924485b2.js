79b1185aaf7742c2de78e19cdaef4270dee99038
Juhi <juhi.iacm@gmail.com>
2024-02-24 21:42:47 +0000

    Updates

---
+++ precache-manifest.6a7aa26c126cd0227cfab963924485b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b2e515c4690d2e59c206aa1e1520562",
+    "url": "/resume_website/index.html"
+  },
+  {
+    "revision": "efa0474f145232db4142",
+    "url": "/resume_website/static/css/main.120e018e.chunk.css"
+  },
+  {
+    "revision": "8500e3c69038c4a92dfa",
+    "url": "/resume_website/static/js/2.889092ad.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/resume_website/static/js/2.889092ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efa0474f145232db4142",
+    "url": "/resume_website/static/js/main.d24c7279.chunk.js"
+  },
+  {
+    "revision": "ee4a773d0b525afa3765",
+    "url": "/resume_website/static/js/runtime-main.4e59809d.js"
+  },
+  {
+    "revision": "03bd9eb3f60ecd9592e8336645aae72b",
+    "url": "/resume_website/static/media/juhi.03bd9eb3.jpeg"
+  }
+]);
